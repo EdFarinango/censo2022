b2127ebb1fa7ac333b6090c1448fdef68cfb9348
EdFarinango <eduardo.farinango@epn.edu.ec>
2022-12-09 16:52:24 -0500

    prueba1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'appmoviles-ba56c',
+    appId: '1:280514763133:web:f3e99168c0010958b91414',
+    storageBucket: 'appmoviles-ba56c.appspot.com',
+    apiKey: 'AIzaSyDExC2r_XHZEh2yIJMW_ZTwJ4GSDwaF8YE',
+    authDomain: 'appmoviles-ba56c.firebaseapp.com',
+    messagingSenderId: '280514763133',
+    measurementId: 'G-VRLX3YR907',
+  },
+  production: true,
+  mapskey: "AIzaSyDvlFYsuoV0T64Nt4yEi0aPVP1QjLDVsvU"
+
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'appmoviles-ba56c',
+    appId: '1:280514763133:web:f3e99168c0010958b91414',
+    storageBucket: 'appmoviles-ba56c.appspot.com',
+    apiKey: 'AIzaSyDExC2r_XHZEh2yIJMW_ZTwJ4GSDwaF8YE',
+    authDomain: 'appmoviles-ba56c.firebaseapp.com',
+    messagingSenderId: '280514763133',
+    measurementId: 'G-VRLX3YR907',
+  },
+  production: false,
+  mapskey: "AIzaSyDvlFYsuoV0T64Nt4yEi0aPVP1QjLDVsvU"
 };
 
 /*
